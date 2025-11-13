7b3466e6e11eafd7e0033f3a293d6950ef0b7eb0
nicolenarvaezm <nicolenar94@gmail.com>
2025-11-13 16:41:32 -0500

    add change password function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -25,7 +25,7 @@ export class UserController {
       const existingUsers = await this.userDao.getAll();
       const emailExist = existingUsers.some(u => u.email === userData.email);
       if (emailExist){
-        res.status(400).json({ error: 'El email ya está registrado' });
+        res.status(400).json({ error: 'The email is already registered' });
         return;
       }
       //encrypt password
@@ -34,7 +34,7 @@ export class UserController {
       const id = await this.userDao.create(userData);
       res.status(201).json({ id });
     } catch (error) {
-      res.status(500).json({ error: 'Error al crear usuario' });
+      res.status(500).json({ error: 'User creating error' });
     }
   }
 
@@ -43,12 +43,12 @@ export class UserController {
       const id = req.params.id;
       const user = await this.userDao.getById(id);
       if (!user) {
-        res.status(404).json({ error: 'Usuario no encontrado' });
+        res.status(404).json({ error: 'Not found user' });
         return;
       }
       res.json(user);
     } catch (error) {
-      res.status(500).json({ error: 'Error al obtener usuario' });
+      res.status(500).json({ error: 'Error retrieving user' });
     }
   }
 
@@ -58,12 +58,12 @@ export class UserController {
       const updateData: Partial<User> = req.body;
       const success = await this.userDao.update(id, updateData);
       if (!success) {
-        res.status(404).json({ error: 'Usuario no encontrado' });
+        res.status(404).json({ error: 'Not found user' });
         return;
       }
-      res.json({ message: 'Usuario actualizado' });
+      res.json({ message: 'Updated user' });
     } catch (error) {
-      res.status(500).json({ error: 'Error al actualizar usuario' });
+      res.status(500).json({ error: 'Error updating user' });
     }
   }
 
@@ -72,10 +72,10 @@ export class UserController {
       const id = req.params.id;
       const success = await this.userDao.delete(id);
       if (!success) {
-        res.status(404).json({ error: 'Usuario no encontrado' });
+        res.status(404).json({ error: 'Not found user' });
         return;
       }
-      res.json({ message: 'Usuario eliminado' });
+      res.json({ message: 'User deleted' });
     } catch (error) {
       res.status(500).json({ error: 'Error al eliminar usuario' });
     }
@@ -86,7 +86,7 @@ export class UserController {
       const users = await this.userDao.getAll();
       res.json(users);
     } catch (error) {
-      res.status(500).json({ error: 'Error al listar usuarios' });
+      res.status(500).json({ error: 'Error listing users' });
     }
   }
 
@@ -95,7 +95,7 @@ export class UserController {
       const { firstName, lastName, age, email, password } = req.body;
 
       if (!firstName || !lastName || !age || !email || !password) {
-        res.status(400).json({ error: "Todos los campos son requeridos" });
+        res.status(400).json({ error: "All fields are required" });
         return;
       }
 
@@ -104,21 +104,20 @@ export class UserController {
         return;
       }
 
-      // Normaliza email
       const normalizedEmail = email.trim().toLowerCase();
 
-      // Verifica que no exista email duplicado
+      // Verify duplicate email
       const existingUser = await this.userDao.userByEmail(normalizedEmail);
       if (existingUser) {
-        res.status(400).json({ error: "El email ya está registrado" });
+        res.status(400).json({ error: "The email is already registered" });
         return;
       }
 
-      // Encripta la contraseña
+      // hash password
       const salt = bcrypt.genSaltSync(10);
       const hashedPassword = bcrypt.hashSync(password, salt);
 
-      // Prepara el objeto usuario para crear
+      // Prepare object to create
       const newUser: User = {
         firstName,
         lastName,
@@ -127,13 +126,13 @@ export class UserController {
         password: hashedPassword,
       };
 
-      // Crea el usuario
+      // Create user 
       const id = await this.userDao.create(newUser);
 
-      res.status(201).json({ message: "Usuario creado", id });
+      res.status(201).json({ message: "User created", id });
     } catch (error) {
-      console.error("Error en registro:", error);
-      res.status(500).json({ error: "Error al registrar usuario" });
+      console.error("registration error:", error);
+      res.status(500).json({ error: "user registration error" });
     }
   }
 
@@ -178,19 +177,17 @@ export class UserController {
 
    async updateUser(req: Request, res: Response): Promise<void> {
     try {
-      // Verificar usuario logueado
+      // Verify user login
       const loggedUser = (req as any).user;
-      console.log(loggedUser)
       if (!loggedUser || !loggedUser.userId) {
-        res.status(401).json({ error: "No autorizado" });
+        res.status(401).json({ error: "Unauthorized" });
         return;
       }
       const userId = loggedUser.userId;
 
-      // Datos a actualizar
+      // Update data
       const updateData = req.body;
 
-      // Permitir solo campos específicos
       const allowedFields: (keyof Pick<User, "email" | "firstName" | "lastName" | "age">)[] = [
         "email",
         "firstName",
@@ -207,7 +204,7 @@ export class UserController {
       }
 
       if (Object.keys(filteredData).length === 0) {
-        res.status(400).json({ error: "No hay campos válidos para actualizar" });
+        res.status(400).json({ error: "There are no valid fields to update" });
         return;
       }
 
@@ -220,14 +217,51 @@ export class UserController {
       const updated = await this.userDao.update(userId, filteredData);
 
       if (!updated) {
-        res.status(404).json({ error: "Usuario no encontrado" });
+        res.status(404).json({ error: "User not found" });
         return;
       }
 
-      res.status(200).json({ message: "Usuario actualizado exitosamente" });
+      res.status(200).json({ message: "user successfully updated" });
     } catch (error) {
-      console.error("Error actualizando usuario:", error);
-      res.status(500).json({ error: "Error al actualizar usuario" });
+      console.error("Error updating user:", error);
+      res.status(500).json({ error: "Error updating user" });
+    }
+  }
+
+  async changePassword(req: Request, res: Response): Promise<void> {
+    try {
+      const loggedUser = (req as any).user;
+      if (!loggedUser || !loggedUser.userId) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+      }
+      const userId = loggedUser.userId;
+
+      const { newPassword, confirmPassword } = req.body;
+
+      if (!newPassword || !confirmPassword) {
+        res.status(400).json({ error: "Both passwords are required" });
+        return;
+      }
+
+      if (newPassword !== confirmPassword) {
+        res.status(400).json({ error: "The passwords do not match" });
+        return;
+      }
+
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+      const updated = await this.userDao.update(userId, { password: hashedPassword });
+
+      if (!updated) {
+        res.status(500).json({ error: "Error updating password" });
+        return;
+      }
+
+      res.status(200).json({ message: "Password successfully updated" });
+    } catch (error) {
+      console.error("Error changing password:", error);
+      res.status(500).json({ error: "Internal error" });
     }
   }
 
@@ -235,7 +269,7 @@ export class UserController {
     try {
       const loggedUser = (req as any).user;
       if (!loggedUser || !loggedUser.userId) {
-        res.status(401).json({ error: "No autorizado" });
+        res.status(401).json({ error: "Unauthorized" });
         return;
       }
       const userId = loggedUser.userId;
@@ -243,14 +277,14 @@ export class UserController {
       const deleted = await this.userDao.delete(userId);
 
       if (!deleted) {
-        res.status(404).json({ error: "Usuario no encontrado" });
+        res.status(404).json({ error: "Not found user" });
         return;
       }
 
-      res.status(200).json({ message: "Usuario eliminado exitosamente" });
+      res.status(200).json({ message: "Delete user successfully" });
     } catch (error) {
-      console.error("Error eliminando usuario:", error);
-      res.status(500).json({ error: "Error al eliminar usuario" });
+      console.error("Error deleting user:", error);
+      res.status(500).json({ error: "Error deleting user" });
     }
   }
 
