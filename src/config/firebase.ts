f870d758a1b54665a3f1555a0d28247c04dbf60b
nicolenarvaezm <nicolenar94@gmail.com>
2025-11-13 22:08:32 -0500

    change in config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,19 +1,25 @@
 import admin from "firebase-admin";
 import path from "path";
+import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const keyPath =
-  process.env.GOOGLE_APPLICATION_CREDENTIALS ||
-  path.join(__dirname, "../../serviceAccountKey.json");
-  
-const serviceAccount = require(path.resolve(keyPath));
+let serviceAccount: any;
+
+//usar variable de entorno json en producción
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+} else {
+  //en local usa el archivo
+  const keyPath = path.join(__dirname, "../../serviceAccountKey.json");
+  const file = fs.readFileSync(keyPath, "utf8");
+  serviceAccount = JSON.parse(file);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
 
 export const db = admin.firestore();
 export const auth = admin.auth();
