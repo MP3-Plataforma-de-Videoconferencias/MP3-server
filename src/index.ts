142a8282fecddd6984adfdfda4e9df640e9b9881
nicolenarvaezm <nicolenar94@gmail.com>
2025-11-17 17:30:54 -0500

    fix2

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
-import emailRoutes from "./routes/emails";
+//import emailRoutes from "./routes/emails";
 
 
 dotenv.config();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
-app.use("/api", emailRoutes);
+//app.use("/api", emailRoutes);
 
 app.get("/", (req, res) => {
   res.send("Servidor corriendo ");
