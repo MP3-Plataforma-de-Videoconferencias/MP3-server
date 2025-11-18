a569acbb69818e90f881783e06ece98250bc8af9
Sara <163233672+Srytha@users.noreply.github.com>
2025-11-18 00:16:56 -0500

    Port change

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
   res.send("Servidor corriendo ");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
